330c00facc2f1f6e3837f6a05d7a17ccd7c78d82
Svetlana Kovakina <skovakina@gmail.com>
2025-03-13 19:45:36 -0700

    add ingredients

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -13,4 +13,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/new", (req, res) => {
+  res.render("ingredients/new.ejs");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const existingIngredient = await Ingredient.findOne({
+      name: req.body.name,
+    });
+
+    if (!existingIngredient) {
+      const newIngredient = new Ingredient({ name: req.body.name });
+      await newIngredient.save();
+    }
+
+    res.redirect("/ingredients");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/ingredients");
+  }
+});
+
 module.exports = router;
