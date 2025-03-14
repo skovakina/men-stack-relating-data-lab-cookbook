5afc806af7537cee677d168f847a519a28bb70fd
Svetlana Kovakina <skovakina@gmail.com>
2025-03-13 19:37:16 -0700

    fix: edit resipes

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -3,73 +3,13 @@ const router = express.Router();
 const Ingredient = require("../models/Ingredient.js");
 
 // Index	/ingredients	GET
-router.get("/ingredients", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const ingredients = await Ingredient.find({});
     res.render("ingredients/index.ejs", { ingredients });
   } catch (error) {
     console.log(error);
     res.redirect("/");
-  }
-});
-
-// New	/ingredients/new	GET
-router.get("/ingredients/new", (req, res) => {
-  res.render("ingredients/new.ejs");
-});
-
-// Create	/ingredients	POST
-router.post("/ingredients", async (req, res) => {
-  try {
-    await Ingredient.create(req.body);
-    res.redirect("/ingredients");
-  } catch (error) {
-    console.log(error);
-    res.redirect("/ingredients/new");
-  }
-});
-
-// Show	/ingredients/:ingredientId	GET
-router.get("/ingredients/:IngredientId", async (req, res) => {
-  try {
-    const Ingredient = await Ingredient.findById(req.params.IngredientId);
-    res.render("ingredients/show.ejs", { Ingredient });
-  } catch (error) {
-    console.log(error);
-    res.redirect("/ingredients");
-  }
-});
-
-// Edit	/ingredients/:ingredientId/edit	GET
-router.get("/ingredients/:IngredientId/edit", async (req, res) => {
-  try {
-    const Ingredient = await Ingredient.findById(req.params.IngredientId);
-    res.render("ingredients/edit.ejs", { Ingredient });
-  } catch (error) {
-    console.log(error);
-    res.redirect("/ingredients");
-  }
-});
-
-// Update	/ingredients/:ingredientId	PUT
-router.put("/ingredients/:IngredientId", async (req, res) => {
-  try {
-    await Ingredient.findByIdAndUpdate(req.params.IngredientId, req.body);
-    res.redirect("/ingredients");
-  } catch (error) {
-    console.log(error);
-    res.redirect("/ingredients");
-  }
-});
-
-// Delete	/ingredients/:ingredientId	DELETE
-router.delete("/ingredients/:IngredientId", async (req, res) => {
-  try {
-    await Ingredient.findByIdAndDelete(req.params.IngredientId);
-    res.redirect("/ingredients");
-  } catch (error) {
-    console.log(error);
-    res.redirect("/ingredients");
   }
 });
 
--- controllers/recipes.js
+++ controllers/recipes.js
@@ -49,7 +49,7 @@ router.get("/:recipeId", async (req, res) => {
       "ingredients"
     );
 
-    if (!recipe) return res.redirect("/recipes"); // If recipe not found, redirect
+    if (!recipe) return res.redirect("/recipes");
 
     res.render("recipes/show.ejs", { recipe });
   } catch (error) {
@@ -73,14 +73,22 @@ router.get("/:recipeId/edit", async (req, res) => {
 });
 
 // Update	/recipes/:recipeId	PUT
-// TODO: it's not finished yet
 
 router.put("/:recipeId", async (req, res) => {
   try {
-    await Recipe.findByIdAndUpdate(req.params.recipeId, req.body);
-    res.redirect("/recipes");
+    const recipe = await Recipe.findById(req.params.recipeId);
+    if (!recipe || !recipe.owner.equals(req.session.user._id)) {
+      return res.redirect("/recipes");
+    }
+    recipe.name = req.body.name;
+    recipe.instructions = req.body.instructions;
+    recipe.ingredients = req.body.ingredients || [];
+
+    await recipe.save();
+
+    res.render("recipes/${recipe._id}", { recipe });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.redirect("/recipes");
   }
 });
@@ -89,10 +97,18 @@ router.put("/:recipeId", async (req, res) => {
 router.delete("/:recipeId", async (req, res) => {
   try {
     const recipe = await Recipe.findById(req.params.recipeId);
+
     if (!recipe || !recipe.owner.equals(req.session.user._id)) {
       return res.redirect("/recipes");
     }
+
     await Recipe.deleteOne({ _id: recipe._id });
+
+    await User.updateOne(
+      { _id: req.session.user._id },
+      { $pull: { recipes: recipe._id } }
+    );
+
     res.redirect("/recipes");
   } catch (error) {
     console.error(error);
